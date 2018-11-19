16c4b8e7470e35d60bd94656305f8cdd0c1c0260
chatelain <4269946@qq.com>
2018-11-19 14:55:15 +0800

    first commit

---
+++ src/main/java/com/touchvirtual/pano_scene_isv/PanoSceneIsvApplication.java
@@ -0,0 +1,12 @@
+package com.touchvirtual.pano_scene_isv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanoSceneIsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PanoSceneIsvApplication.class, args);
+    }
+}
